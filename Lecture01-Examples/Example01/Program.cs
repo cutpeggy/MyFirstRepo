f9cb35e0ead46ac4643b07ca2fd3b0db523cb454
cutpeggy <cutpeggy@gmail.com>
2017-06-27 00:14:08 +0800

    Add Lecture01 - Example01

---
+++ Lecture01-Examples/Example01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student aStudent = new Student()
+            {
+                Name = "123",
+                Phone = "0912345678",
+                Address = "長庚蘊德"
+            };
+
+            Console.WriteLine(aStudent.Name);
+        }
+    }
+}
