2a70e908e473954f3025b85a27128cc5fe70f79f
cutpeggy <cutpeggy@gmail.com>
2017-06-27 23:26:39 +0800

    Update Lecture01-Examples

---
+++ Lecture01-Examples/Example1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student aStudent = new Student()
+            {
+                Name = "張元鴻",
+                Phone = "0910123456",
+                Address = "台大資工"
+            };
+
+            Console.WriteLine(aStudent.Name);
+            Console.WriteLine(aStudent.Address);
+        }
+    }
+}
---
+++ Lecture01-Examples/Example3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example1;
+
+namespace Example3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.IsHappy = true;
+
+            if (student.IsHappy)
+            {
+                Console.WriteLine("開心");
+            }
+            else
+            {
+                Console.WriteLine("不開心");
+            }
+        }
+    }
+}
